40e947e0b07f0cf24bd126d9cbb9591cd4d66e2d
juanpablomoscoloni <juampi9611@gmail.com>
2019-06-27 21:49:49 -0300

    Todo subido

---
+++ Asistra/app/src/main/java/clases/Alumno.java
@@ -0,0 +1,33 @@
+package clases;
+
+public class Alumno {
+
+    private String nombre;
+    private String apellido;
+    private String legajo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(String legajo) {
+        this.legajo = legajo;
+    }
+
+}
---
+++ Asistra/app/src/main/java/com/example/asistra/LoginActivity.java
@@ -0,0 +1,166 @@
+package com.example.asistra;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public EditText campoUsuario;
+    public EditText campoContra;
+    public ProgressBar progreso;
+    public Button iniciar;
+
+   // public Usuario usuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        campoContra = findViewById(R.id.pass);
+        campoUsuario = findViewById(R.id.usuario);
+        progreso = findViewById(R.id.inicioProgreso);
+        iniciar = findViewById(R.id.iniciar);
+
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (comprobarCampos()) {
+                    iniciarSesion();
+                }
+
+            }
+        });
+
+    }
+
+    public Boolean comprobarCampos () {
+
+        Boolean resultado=true;
+
+        if (campoUsuario.getText().toString().equals("") || campoContra.getText().toString().equals("")) {
+
+            Toast.makeText(getApplicationContext(), "Llenar los campos", Toast.LENGTH_LONG).show();
+            resultado = false;
+        }
+
+
+        return resultado;
+    }
+
+    public void iniciarSesion () {
+
+        //Bloquea el botón de Inicio de Sesión
+        iniciar.setEnabled(false);
+
+        //Empieza a girar la ruedita
+        progreso.setVisibility(View.VISIBLE);
+
+        String urlConsulta = "http://192.168.43.218/proyectoAsistencia/comprobarUsuario.php";
+
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlConsulta, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+
+                try {
+
+                    JSONObject jsonobject = new JSONObject(response);
+                    JSONArray jsonArray = jsonobject.getJSONArray("Usuario"); //Modificable
+
+                    if (jsonArray.length() > 0) {
+
+                        JSONObject user = jsonArray.getJSONObject(0); //Modificable
+
+                        if (user.getString("rol").equals("alumno")) {
+                            pasarActividadAlumno(user.getString("id"));
+                        } else {
+
+                        }
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "No se encontró", Toast.LENGTH_LONG).show();
+                    }
+
+                    progreso.setVisibility(View.GONE);
+                    iniciar.setEnabled(true);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Ocurrio un error", Toast.LENGTH_LONG).show();
+                    progreso.setVisibility(View.GONE);
+                    iniciar.setEnabled(true);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Sin conexion", Toast.LENGTH_LONG).show();
+                progreso.setVisibility(View.GONE);
+                iniciar.setEnabled(true);
+            }
+        }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+
+                parametros.put("user", campoUsuario.getText().toString());
+                parametros.put("password", campoContra.getText().toString());
+
+                return parametros;
+
+            }
+
+        };
+
+        requestQueue.add(stringRequest);
+
+    }
+
+   public void pasarActividadAlumno (String id) {
+
+     Intent intent = new Intent(LoginActivity.this, TokenActivity.class);
+
+     intent.putExtra("id", id);
+     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+     startActivity(intent);
+
+    }
+
+    public void pasarActividadDocente (String id) {
+
+        Intent intent = new Intent(LoginActivity.this, TokenActivity.class);
+
+        intent.putExtra("id", id);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(intent);
+
+    }
+}
