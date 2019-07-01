c7ca60abc9c5151f7b920aaf360c261e05e0adba
azamma <azamma@users.noreply.github.com>
2019-07-01 11:16:34 -0300

    Se modificó algo del manifest

---
+++ Asistra/app/src/main/java/com/example/asistra/UltimosPresentesActivity.java
@@ -0,0 +1,13 @@
+package com.example.asistra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UltimosPresentesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ultimos_presentes);
+    }
+}
